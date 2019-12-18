2626906668627fd90508ce77ec763c6821599bc1
saurabhguptarock <saurabhguptarock119@gmail.com>
2019-12-18 15:14:47 +0530

    .

--- lib/shared/adunit_id.dart
+++
@@ -1,8 +0,0 @@
-// String bannerAdId = 'ca-app-pub-3723052345438421/5226780879';
-// String interstitialAdId = 'ca-app-pub-3723052345438421/1131655012';
-// String rewardedAdId = 'ca-app-pub-3723052345438421/8502060337';
-// String appId = 'ca-app-pub-3723052345438421~3345000795';
-String bannerAdId;
-String interstitialAdId;
-String rewardedAdId;
-String appId;
